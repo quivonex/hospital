3ca9490022b281a05377d56fe018836b6a56ba65
Amey <ameyandurekar@gmail.com>
2025-08-30 16:44:03 +0530

    update message

--- CARETAKER/hospital/hospital/settings.py
+++ CARETAKER/hospital/hospital/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'caredb',
         'USER':'root',
-        'PASSWORD':'root',
+        'PASSWORD':'root@12345!',
         'HOST':'localhost',
         'PORT':'3306',
 
